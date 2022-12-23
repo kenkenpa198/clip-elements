312b63ad63a369bc3db2cce9d3381d3512dd55ae
kenkenpa198 <kenkenpa198@gmail.com>
2022-12-24 07:51:25 +0900

    [update]例外処理を実装
    以下の場合にアラートを表示するように更新。
    
    - 定数 `GET_CLASS_NAME` で指定されているクラス要素がページ上に存在しなかった場合。
    - クリップボードへの書き込みに失敗した場合。
    
    この他変数宣言などを調整。

--- clip-elements-source.js
+++ clip-elements-source.js
@@ -2,18 +2,32 @@
 javascript:(
     function(){
         // 要素を取得
-        const ELEMENTS = document.getElementsByClassName('a-truncate-cut');
+        const GET_CLASS_NAME = 'mw-headline';
+        let elements = document.getElementsByClassName(GET_CLASS_NAME);
+
+        // 要素が取得できていない場合はアラートを表示して終了
+        if (!(elements.length)) {
+            alert('要素を取得できませんでした。\nコード内で指定されているクラス要素がページ上に存在するか確認してください。');
+            return 1;
+        }
 
         // 要素内のテキストを配列化
         let texts = [];
-        for (let i = 0; i < ELEMENTS.length; i++) {
-            texts.push(ELEMENTS[i].innerText);
+        for (let i = 0; i < elements.length; i++) {
+            texts.push(elements[i].innerText);
         }
 
         // 配列を連結
         let out_string = texts.join(',\n');
 
         // クリップボードへコピーする
-        navigator.clipboard.writeText(out_string);
+        try {
+            navigator.clipboard.writeText(out_string);
+        } catch (e) {
+            alert('クリップボードへの書き込みに失敗しました。\n' + e);
+            return 1
+        }
+
+        return 0;
     }
 )();
