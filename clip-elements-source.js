a78e7020b11a53bebb6c98d8d39d42d49cb08c46
kenkenpa198 <kenkenpa198@gmail.com>
2022-12-28 00:43:38 +0900

    [update]戻り値と変数名を微修正

--- clip-elements-source.js
+++ clip-elements-source.js
@@ -8,7 +8,7 @@ javascript:(
         // 要素が取得できていない場合はアラートを表示して終了
         if (!(elements.length)) {
             alert('要素を取得できませんでした。\nコード内で指定されているクラス要素がページ上に存在するか確認してください。');
-            return 1;
+            return;
         }
 
         // 要素内のテキストを配列化
@@ -18,16 +18,16 @@ javascript:(
         }
 
         // 配列をカンマ・改行区切りで連結
-        let out_string = texts.join(',\n') + '\n';
+        let result = texts.join(',\n') + '\n';
 
         // クリップボードへコピーする
         try {
-            navigator.clipboard.writeText(out_string);
+            navigator.clipboard.writeText(result);
         } catch (e) {
             alert('クリップボードへの書き込みに失敗しました。\n' + e);
-            return 1
+            return;
         }
 
-        return 0;
+        return;
     }
 )();
